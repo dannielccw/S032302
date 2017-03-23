8d1bf13d9755151808ac74f970f9e9fe0ca05176
dannielccw <dannielccw@gmail.com>
2017-03-23 10:10:15 +0800

    setSingleChoiceItems 1

--- app/src/main/java/com/example/yvtc/s032302/MainActivity.java
+++ app/src/main/java/com/example/yvtc/s032302/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import static com.example.yvtc.s032302.R.styleable.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,67 +17,88 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-       public void click1(View v)
-       {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("對話框測試");
-                builder.setMessage("這是一個對話框測試");
-               builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
-                        @Override
-                       public void onClick(DialogInterface dialog, int which) {
-                               Log.d("DLG", "使用者按下確定");
-                            }
-                    });
-                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                               Log.d("DLG", "使用者按下取消");
-                            }
-                });
 
-                builder.setNeutralButton("看說明", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Log.d("DLG", "使用者按下看說明");
-                            }
-                        });
-
-               builder.show();
+    public void click1(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("對話框測試");
+        builder.setMessage("這是一個對話框測試");
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("DLG", "使用者按下確定");
             }
-
-            public void click2(View v)
-            {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("輸入測試");
-                builder.setMessage("請輸入訊息:");
-                builder.setIcon(android.R.drawable.ic_dialog_info);
-                final TextView tv= (TextView) findViewById(R.id.textView);
-                final EditText ed = new EditText(MainActivity.this);
-                builder.setView(ed);
-
-                builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Log.d("DLG", "使用者按下確定");
-                        tv.setText(ed.getText().toString());
-                    }
-                });
-                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Log.d("DLG", "使用者按下取消");
-                    }
-                });
-
-                builder.show();
-
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("DLG", "使用者按下取消");
             }
+        });
 
+        builder.setNeutralButton("看說明", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("DLG", "使用者按下看說明");
+            }
+        });
 
+        builder.show();
+    }
 
+    public void click2(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("輸入測試");
+        builder.setMessage("請輸入訊息:");
+        builder.setIcon(android.R.drawable.ic_dialog_info);
+        final TextView tv = (TextView) findViewById(R.id.textView);
+        final EditText ed = new EditText(MainActivity.this);
+        builder.setView(ed);
 
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("DLG", "使用者按下確定");
+                tv.setText(ed.getText().toString());
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("DLG", "使用者按下取消");
+            }
+        });
 
-
-
+        builder.show();
 
     }
+
+
+    public void click3(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("選項測試");
+        final String fruits[] = getResources().getStringArray(R.array.fruits);
+        builder.setSingleChoiceItems(R.array.fruits, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                TextView tv2 = (TextView) findViewById(R.id.textView2);
+                tv2.setText(fruits[which]);
+            }
+        });
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("DLG", "使用者按下確定");
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("DLG", "使用者按下取消");
+            }
+        });
+
+        builder.show();
+    }
+
+
+}
